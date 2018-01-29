2517115d26c42d6c7535e0207b444f09261ead15
shayhewa <35606020+shayhewa@users.noreply.github.com>
2018-01-28 19:42:55 -0800

    CS 633 - Week 1
    Commit base project and data scripts for database

---
+++ insert for Electives.sql
@@ -0,0 +1,188 @@
+﻿insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	544,
+	'Foundation of Analytics',
+	'The goal of this course is to provide students with the mathematical and practical background required in the field of data analytics. Starting with an introduction to probability and statistics, the R tool is introduced for statistical computing and graphics. Different types of data are investigated along with data summarization techniques and plotting. Data populations using discrete, continuous, and multivariate distributions are explored. Errors during measurements and computations are analyzed in the course. Confidence intervals and hypothesis testing topics are also examined. The concepts covered in the course are demonstrated using R. Laboratory Course. Prereq: MET CS 546 or equivalent knowledge, or instructor''s consent.',
+	'Fall 2018',
+	'MCIS',
+	NULL,
+	NULL
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	546,
+	'Quantative Methods for Information Systems',
+	'The goal of this course is to provide Computer Information Systems students with the mathematical fundamentals required for successful quantitative analysis of problems in the field of business computing. The first part of the course introduces the mathematical prerequisites for understanding probability and statistics. Topics include combinatorial mathematics, functions, and the fundamentals of differentiation and integration. The second part of the course concentrates on the study of elementary probability theory, discrete and continuous distributions. Prereq: Academic background that includes the material covered in a standard course on college algebra or instructor''s consent.',
+	'Spring 2018',
+	'MCIS',
+	NULL,
+	NULL
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	555,
+	'Data Analysis and Visualization',
+	'This course provides an overview of the statistical tools most commonly used to process, analyze, and visualize data. Topics include simple linear regression, multiple regression, logistic regression, analysis of variance, and survival analysis. These topics are explored using the statistical package R, with a focus on understanding how to use and interpret output from this software as well as how to visualize results. In each topic area, the methodology, including underlying assumptions and the mechanics of how it all works along with appropriate interpretation of the results, are discussed. Concepts are presented in context of real world examples. Recommended Prerequisite: MET CS 544 or equivalent knowledge, or instructor''s consent.',
+	'Spring 2018',
+	'MCIS',
+	544,
+	NULL
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	632,
+	'IT Project Management',
+	'This course provides students with a comprehensive overview of the principles, processes, and practices of software project management. Students learn techniques for planning, organizing, scheduling, and controlling software projects. There is substantial focus on software cost estimation and software risk management. Students will obtain practical project management skills and competencies related to the definition of a software project, establishment of project communications, managing project changes, and managing distributed software teams and projects.',
+	'Fall 2018',
+	'MCIS',
+	NULL,
+	NULL
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	633,
+	'Software Quality, Testing, & Security Management',
+	'Theory and practice of security and quality assurance and testing for each step of the software development cycle. Verification vs. validation. Test case design techniques, test coverage criteria, security development and verification practices, and tools for static and dynamic analysis. Standards. Test-driven development. QA for maintenance and legacy applications. From a project management knowledge perspective, this course covers the methods, tools and techniques associated with the following processes -- Plan Quality, Perform Quality Assurance, and Perform Quality Control. Prereq: MET CS 520 or 521. Or instructor''s consent.',
+	'Spring 2018',
+	'MCIS',
+	520,
+	521
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	634,
+	'Agile Software Development',
+	'This course provides students with a comprehensive overview of the principles, processes, and practices of agile software development. Students learn techniques for initiating, planning and executing on software development projects using agile methodologies. Students will obtain practical knowledge of agile development frameworks and be able to distinguish between agile and traditional project management methodologies. Students will learn how to apply agile tools and techniques in the software development lifecycle from project ideation to deployment, including establishing an agile team environment, roles and responsibilities, communication and reporting methods, and embracing change. We also leverage the guidelines outlined by the Project Management Institute for agile project development as a framework in this course.',
+	'Fall 2018',
+	'MCIS',
+	231,
+	232
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	688,
+	'Web Analytics and Mining',
+	'The Web Analytics and Mining course covers the areas of web analytics, text mining, web mining, and practical application domains. The web analytics part of the course studies the metrics of web sites, their content, user behavior, and reporting. Google analytics tool is used for collection of web site data and doing the analysis. The text mining module covers the analysis of text including content extraction, string matching, clustering, classification, and recommendation systems. The web mining module studies how web crawlers process and index the content of web sites, how search works, and how results are ranked. Application areas mining the social web and game metrics will be extensively investigated. Laboratory Course. Prerequisites: MET CS 544, or MET CS 555 or equivalent knowledge, or instructor''s consent.',
+	'Spring 2018',
+	'MCIS',
+	544,
+	555
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	699,
+	'Data Mining',
+	'The goal of this course is to study basic concepts and techniques of data mining. The topics include data preparation, classification, performance evaluation, association rule mining, and clustering. We will discuss basic data mining algorithms in the class and students will practice data mining techniques using data mining software. Students will use Weka and SQL Server or Oracle. Prereq: MS CS Prerequisites: MET CS 579; or instructor''s consent. MS CIS Prerequisites: MET CS 669 and MET CS 546; or instructor''s consent.',
+	'Fall 2018',
+	'MCIS',
+	546,
+	669
+)
+
+insert into Electives
+(
+	CourseCode,
+	CourseName,
+	CourseDescription,
+	Semesters,
+	ProgramName,
+	Prerequisite1,
+	Prerequisite2
+)
+Values
+(
+	783,
+	'Enterprise Architecture',
+	'This course builds upon the strong technical foundation of our MSCIS and MSCS curricula, by providing students with the CIO-level management perspective and skills of an enterprise architect, in the context of the technologies that implement those architectures. Our Ross, Weil, and Robertson text provides much of the management content of the course, and the online and classroom content provide both management and technical skills. Students learn that enterprise architectures are best developed incrementally, by system development projects that are aligned with strategic goals and the enterprise architecture. The online content therefore includes many real enterprise system development case studies, showing how these enterprise systems contributed to and helped define the overall enterprise architecture. The course also includes a number of realistic enterprise architecture assignments and an incremental term project with components spanning the course, to provide students with hands on enterprise architecture experience. The course provides students with the understanding and skills needed to define and implement successful enterprise architectures that provide real value to organizations, such as substantially reducing IT costs while improving performance, agility and alignment of information technology to business goals. Prereq: MET CS 682. Or strategic IT experience. Or instructor''s consent. 4 credits.',
+	'Spring 2018',
+	'MCIS',
+	248,
+	NULL
+)
